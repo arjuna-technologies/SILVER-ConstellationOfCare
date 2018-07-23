7ba1d607f40084ce61037c413ad6a326cd9082f6
Stuart Wheater <stuart.wheater@arjuna.com>
2018-07-23 21:02:30 +0100

    Restructured

--- src/app/report/health-diary/health-diary.component.ts
+++ src/app/report/health-diary/health-diary.component.ts
@@ -6,74 +6,83 @@ import { MIGEvent } from '../../mig/mig-event';
 
 declare var google: any;
 
-@Component({
-  selector: 'cnstll-health-diary',
-  templateUrl: './health-diary.component.html',
-  styleUrls: ['./health-diary.component.scss']
+@Component
+({
+    selector:    'cnstll-health-diary',
+    templateUrl: './health-diary.component.html',
+    styleUrls:   ['./health-diary.component.scss']
 })
 export class HealthDiaryComponent implements AfterViewInit, OnChanges
 {
-  @ViewChild('eventsCalendar')
-  private calendar;
+    @ViewChild('eventsCalendar')
+    private calendar;
 
-  @Input('events')
-  private events: MIGEvent[];
+    @Input('events')
+    private events: MIGEvent[];
 
-  private chart;
-  private dataTable;
-  private options;
-  private safeToDraw = false;
+    private chart;
+    private dataTable;
+    private options;
+    private googleChartsSetup: boolean;
 
-  public constructor(private googleChartsLoaderService: GoogleChartsLoaderService)
-  {
-  }
+    public constructor(private googleChartsLoaderService: GoogleChartsLoaderService)
+    {
+        this.googleChartsSetup = false;
+    }
 
-  public ngAfterViewInit(): void
-  {
-    this.googleChartsLoaderService.load()
-      .then(() => { this.setupEvents(); this.safeToDraw=true; this.drawChart() });
-  }
+    public ngAfterViewInit(): void
+    {
+        this.googleChartsLoaderService.load()
+            .then(() => { this.setupEvents(); this.populateDataTable(); this.drawChart() });
+    }
 
-  private populateDataTable(): void {
-    if (this.dataTable) {
-      //this.dataTable.clear();
-      for (let event of this.events) {
-        let eventDate = new Date(event.effectiveTime);
-        if (eventDate > new Date(1970,0,1)) {
-          this.dataTable.addRow([eventDate, 1]);
+    public ngOnChanges(): void
+    {
+        if (this.googleChartsSetup)
+        {
+            this.populateDataTable();
+            this.drawChart();
         }
-      }
     }
-  }
 
-  public ngOnChanges(): void {
-    this.populateDataTable();
-    if (this.safeToDraw) {
-      this.drawChart();
+    private populateDataTable(): void
+    {
+        if (this.googleChartsSetup)
+        {
+            this.dataTable = new google.visualization.DataTable();
+            this.dataTable.addColumn({ type: 'date', id: 'Date' });
+            this.dataTable.addColumn({ type: 'number', id: 'Event' });
+
+            for (let event of this.events)
+            {
+                let eventDate = new Date(event.effectiveTime);
+
+                console.log('Date:      ', eventDate);
+
+                if (eventDate > new Date(2000,0,1))
+                    this.dataTable.addRow([eventDate, 1]);
+            }
+        }
     }
-  }
 
-  private setupEvents(): void
-  {
-    this.chart = new google.visualization.Calendar(this.calendar.nativeElement);
-    this.options = {
-      title: "Health Diary",
-      width: 1200,
-      height: 800,
-    };
-    this.dataTable = new google.visualization.DataTable();
-    this.dataTable.addColumn({ type: 'date', id: 'Date' });
-    this.dataTable.addColumn({ type: 'number', id: 'Event' });
-    this.populateDataTable();
-    google.visualization.events.addListener(this.chart, 'select', () => this.selectHandler());
-  }
+    private setupEvents(): void
+    {
+        this.googleChartsSetup = true;
 
-  private drawChart(): void
-  {
-    this.chart.draw(this.dataTable, this.options);
-  }
+        this.chart = new google.visualization.Calendar(this.calendar.nativeElement);
+        this.options =
+        {
+        };
+        google.visualization.events.addListener(this.chart, 'select', () => this.selectHandler());        
+    }
 
-  private selectHandler(): void
-  {
-  }
+    private drawChart(): void
+    {
+        console.log('Data Table ', this.dataTable);
+        this.chart.draw(this.dataTable, this.options);
+    }
+
+    private selectHandler(): void
+    {
+    }
 }
